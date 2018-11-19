a31446aaa296588d3b6aa27b221218c5cf0c899f
Aimad <aimad.takhtoukh@gmail.com>
2018-11-19 01:48:14 +0100

    Free Cors Configuration

--- back/src/main/java/fr/iai/planner/config/CorsConfig.java
+++ back/src/main/java/fr/iai/planner/config/CorsConfig.java
@@ -3,14 +3,16 @@ package fr.iai.planner.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("**");
+        registry.addMapping("/**");
     }
 
     @Bean
